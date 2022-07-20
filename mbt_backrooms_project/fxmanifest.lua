b4bc124cac972858911ba47607ea5a005767739c
Iakkoise <63969450+AndreIakko@users.noreply.github.com>
2022-07-21 00:33:38 +0200

    Add files via upload

---
+++ mbt_backrooms_project/fxmanifest.lua
@@ -0,0 +1,24 @@
+fx_version "cerulean"
+
+game "gta5"
+
+description "mbt_backrooms_project"
+
+discord "https://discord.gg/tqk3kAEr4f"
+
+this_is_a_map 'yes'
+
+
+client_scripts {
+  "config.lua",
+  "client.lua"
+}
+
+files {
+  "interiorproxies.meta"
+}
+
+
+-- credits:
+--           Maps    Iakko
+--           Script  DarkSideOfTheCode
